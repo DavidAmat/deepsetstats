ee37630d61c7e120fe6a66a74b6c4ceae2becc4c
DavidAmat <daolondrizdaolondriz@gmail.com>
2022-10-30 13:15:33 +0100

    line detection with masking

--- deepsetstats/utils/constants.py
+++ deepsetstats/utils/constants.py
@@ -1,7 +1,48 @@
 # from os.path import join
 # from pathlib import Path
+import numpy as np
 
 # -------------------------
 #      Paths
 # -------------------------
 PATH_VIDS = "/tmp/youtube"
+
+# -------------------------
+#      RGB Courts
+# -------------------------
+RGB_CLAY = np.array((183, 115, 84))
+RGB_US = np.array((100, 115, 159))
+RGB_WBl = np.array((154, 152, 114))
+RGB_WBd = np.array((137, 133, 88))
+RGB_AO = np.array((108, 156, 196))
+RGB_COURTS = np.vstack((RGB_CLAY, RGB_US, RGB_WBl, RGB_WBd, RGB_AO))
+LIST_COURTS = ["CLAY", "GREENSET_US", "GRASS", "GRASS", "GREENSET_AO"]
+
+# -------------------------
+#      RGB Lines
+# -------------------------
+TOL_LINE = np.array([15, 10, 10])
+RGB_LINE_CLAYc = np.array((234, 177, 153))
+RGB_LINE_CLAYd = np.array((204, 140, 112))
+RGB_LINE_GREENSET_US = np.array((231, 233, 238))
+RGB_LINE_GRASSc = np.array((244, 232, 226))
+RGB_LINE_GRASSd = np.array((228, 199, 196))
+RGB_LINE_GREENSET_AO = np.array((200, 228, 240))
+
+# -------------------------
+#      Gradient Lines
+# -------------------------
+D_GRAD_LINES = {
+    "CLAY": {"COURT": RGB_CLAY, "LINE_CLEAN": RGB_LINE_CLAYc, "LINE_DIRTY": RGB_LINE_CLAYd},
+    "GREENSET_US": {
+        "COURT": RGB_US,
+        "LINE_CLEAN": RGB_LINE_GREENSET_US,
+        "LINE_DIRTY": RGB_LINE_GREENSET_US,
+    },
+    "GRASS": {"COURT": RGB_WBd, "LINE_CLEAN": RGB_LINE_GRASSc, "LINE_DIRTY": RGB_LINE_GRASSd},
+    "GREENSET_AO": {
+        "COURT": RGB_AO,
+        "LINE_CLEAN": RGB_LINE_GREENSET_AO,
+        "LINE_DIRTY": RGB_LINE_GREENSET_AO,
+    },
+}
--- deepsetstats/utils/videocapture.py
+++ deepsetstats/utils/videocapture.py
@@ -1,5 +1,8 @@
 import cv2
+import numpy as np
 from matplotlib import pyplot as plt
+
+from deepsetstats.utils import constants as c
 
 
 class VideoClass:
@@ -24,6 +27,41 @@ class VideoClass:
         else:
             frame_corr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         return frame_corr
+
+    @staticmethod
+    def detect_court(img):
+        # Crop image from center
+        center = np.array(img.shape) / 2
+
+        # Try different crops
+        mean_rgb = []
+        for size_crop in [100, 200, 300, 500]:
+            x = center[1] - size_crop / 2
+            y = center[0] - size_crop / 2
+            crop_img = img[int(y) : int(y + size_crop), int(x) : int(x + size_crop)]
+            mean_rgb.append(list(np.mean(np.mean(crop_img, axis=0), axis=0)))
+        mRGB = np.mean(np.array(mean_rgb), axis=0)
+        meanRGB_diff = np.mean(np.abs(mRGB - c.RGB_COURTS), axis=1)
+        COURT_TYPE = c.LIST_COURTS[np.argmin(meanRGB_diff)]
+        return mRGB, COURT_TYPE
+
+    @staticmethod
+    def get_line_mask(self, img, court_type):
+        # ------------------------- #
+        #     Line Tolerance
+        # ------------------------- #
+        RGB_LINE_DIRTY_TOL = c.D_GRAD_LINES[court_type]["LINE_DIRTY"] - c.TOL_LINE
+
+        # ------------------------- #
+        #     Get Mask
+        # ------------------------- #
+        # set the lower and upper bounds for the green hue
+        lower_white = RGB_LINE_DIRTY_TOL
+        upper_white = np.array([255, 255, 255])
+
+        # create a mask for green colour using inRange function
+        mask = cv2.inRange(img, lower_white, upper_white)
+        return mask
 
     def get_num_frames(self):
         return int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
