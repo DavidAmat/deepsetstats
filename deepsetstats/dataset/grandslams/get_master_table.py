a5a010f071f7708287def0d17d8d52f5c348d09e
DavidAmat <daolondrizdaolondriz@gmail.com>
2023-08-25 16:43:34 +0200

    feat: copying dataset with grandslams to harddrive

---
+++ deepsetstats/dataset/grandslams/get_master_table.py
@@ -0,0 +1,125 @@
+import os
+
+import pandas as pd
+import tqdm
+
+from deepsetstats.dataset.utils import (
+    extract_players_title,
+    extract_tournament_title,
+    load_pickle,
+)
+
+os.system("clear")
+
+# Path dataframe
+PATH_US_OPEN_VIDEOS = "deepsetstats/dataset/grandslams/parquet/us_open_videos.parquet"
+PATH_AUSTRALIA_OPEN_VIDEOS = "deepsetstats/dataset/grandslams/parquet/australia_open_videos.parquet"
+PATH_ROLANDGARROS_VIDEOS = "deepsetstats/dataset/grandslams/parquet/roland_garros_videos.parquet"
+PATH_WIMBLEDON_VIDEOS = "deepsetstats/dataset/grandslams/parquet/wimbledon_videos.parquet"
+PATH_TOURNAMENTS_NAMING = (
+    "deepsetstats/dataset/tournaments/parquet/tournaments_tennistv.parquet"
+)
+
+PATH_BIBLE_PLAYERS = "deepsetstats/dataset/players/parquet/bible_players.parquet"
+PATH_MAP_ID2NAME = "deepsetstats/dataset/players/pickle/map_id2name.parquet"
+
+# Output Path
+PATH_MASTER = "deepsetstats/dataset/grandslams/parquet/master.parquet"
+
+# --------------------------------------- #
+# --------------------------------------- #
+# Get datasets
+# --------------------------------------- #
+# --------------------------------------- #
+df_tour = pd.read_parquet(PATH_TOURNAMENTS_NAMING, engine="pyarrow")
+df_us = pd.read_parquet(PATH_US_OPEN_VIDEOS, engine="pyarrow")
+df_rg = pd.read_parquet(PATH_ROLANDGARROS_VIDEOS, engine="pyarrow")
+df_ao = pd.read_parquet(PATH_AUSTRALIA_OPEN_VIDEOS, engine="pyarrow")
+df_wb = pd.read_parquet(PATH_WIMBLEDON_VIDEOS, engine="pyarrow")
+df_players = pd.read_parquet(PATH_BIBLE_PLAYERS, engine="pyarrow")
+df_players = df_players[df_players["best_ranking"] < 100].copy()
+d1 = load_pickle(PATH_MAP_ID2NAME)
+
+
+df_us["tournament_name"] = "US Open"
+df_rg["tournament_name"] = "Roland Garros"
+df_ao["tournament_name"] = "Australian Open"
+df_wb["tournament_name"] = "Wimbledon"
+
+# --------------------------------- #
+# Players
+# --------------------------------- #
+player_common_names = df_players["common_name"].tolist()
+player_common_names_2 = df_players["common_name_2"].tolist()
+player_full_names = df_players["name"].tolist()
+player_ids = df_players["player_id"].tolist()
+
+# --------------------------------- #
+# Ids of Grand Slams
+# --------------------------------- #
+d_ids_grandslams = {}
+for _, row in df_tour[df_tour["level"] == "grandslam"].iterrows():
+    d_ids_grandslams[row["tournament_name"]] = row["tournament_id"]
+
+# --------------------------------- #
+# Tennis TV Get Videos of Highlights
+# --------------------------------- #
+df_all_grands = pd.concat([df_us, df_rg, df_ao, df_wb])
+mask1 = df_all_grands.title.str.contains("ighlights")
+mask2 = df_all_grands.title.str.contains("Condensed Match")
+mask3 = df_all_grands.title.str.contains("Full Match")
+dfh = df_all_grands[mask1 | mask2 | mask3]
+
+# --------------------------------------- #
+# --------------------------------------- #
+# Create master table
+# --------------------------------------- #
+# --------------------------------------- #
+ept = extract_players_title
+ett = extract_tournament_title
+
+d_master = {
+    "video_id": [],
+    "player_id": [],
+    "tournament_id": [],
+}
+not_found_tourn = []
+not_found_players = []
+
+
+for _, row in tqdm.tqdm(dfh.iterrows()):
+    video_id = row["video_id"]
+    raw_title = row["title"]
+    tour_name = row["tournament_name"]
+    tour_id = d_ids_grandslams[tour_name]
+
+    play_ids = ept(
+        raw_string=raw_title,
+        player_ids=player_ids,
+        player_full_names=player_full_names,
+        player_common_names=player_common_names,
+        player_common_names_2=player_common_names_2,
+    )
+
+    if len(play_ids) < 1:
+        not_found_players.append(video_id)
+        continue
+
+    for pid in play_ids:
+        d_master["video_id"].append(video_id)
+        d_master["player_id"].append(pid)
+        d_master["tournament_id"].append(tour_id)
+
+df_final = pd.DataFrame(d_master)
+
+cols_tour = ["tournament_name", "tournament_id"]
+df_final = pd.merge(df_final, df_tour[cols_tour], on="tournament_id", how="left")
+
+cols_players = ["player_id", "name"]
+df_final = pd.merge(df_final, df_players[cols_players], on="player_id", how="left")
+
+cols_tt = ["title", "video_id"]
+df_final = pd.merge(df_final, dfh[cols_tt], on="video_id", how="left")
+
+# Writing final dataframe master
+df_final.to_parquet(PATH_MASTER, engine="pyarrow")
---
+++ deepsetstats/dataset/grandslams/get_wimbledon_videos.py
@@ -0,0 +1,80 @@
+import os
+import time
+
+import pandas as pd
+from googleapiclient.discovery import build
+
+os.chdir("/usr/src/app")
+
+# from deepsetstats.dataset.utils import master_prettify
+
+# Replace 'YOUR_API_KEY' with your actual YouTube API key
+API_KEY = os.environ["API_KEY"]
+
+# Tennis TV Channel
+CHANNEL_ID = "UCNa8NxMgSm7m4Ii9d4QGk1Q"
+
+# Path dataframe
+PATH_WIMBLEDON_VIDEOS = "deepsetstats/dataset/grandslams/parquet/wimbledon_videos.parquet"
+
+# Create a YouTube API client
+youtube = build("youtube", "v3", developerKey=API_KEY)
+
+# Channels contents
+channel_response = (
+    youtube.channels().list(id=CHANNEL_ID, part="contentDetails").execute()
+)
+
+# Get playlist ID for uploads of the channel
+uploads_playlist_id = channel_response["items"][0]["contentDetails"][
+    "relatedPlaylists"
+]["uploads"]
+
+# Get all videos from the uploads playlist
+video_info = []
+next_page_token = None
+pages = 7600 // 50
+
+for page in range(0, int(pages) + 1):
+    print(f"Page: {page}")
+    playlist_items_response = (
+        youtube.playlistItems()
+        .list(
+            playlistId=uploads_playlist_id,
+            part="contentDetails",
+            maxResults=50,  # Maximum value allowed by API
+            pageToken=next_page_token,
+        )
+        .execute()
+    )
+
+    playlist_items = playlist_items_response["items"]
+    video_ids = [item["contentDetails"]["videoId"] for item in playlist_items]
+
+    video_response = (
+        youtube.videos().list(id=",".join(video_ids), part="snippet").execute()
+    )
+
+    for item in video_response["items"]:
+        snippet = item["snippet"]
+        video_info.append(
+            {
+                "title": snippet["title"],
+                "video_id": item["id"],
+                "date_publish": snippet["publishedAt"],
+            }
+        )
+
+    next_page_token = playlist_items_response.get("nextPageToken")
+
+    # Break the loop if there are no more pages to retrieve
+    if not next_page_token:
+        break
+
+    if (page % 10) == 0:
+        # Sleep
+        print("sleeping...")
+        time.sleep(3)
+
+df = pd.DataFrame(video_info)
+df.to_parquet(PATH_WIMBLEDON_VIDEOS, engine="pyarrow")
